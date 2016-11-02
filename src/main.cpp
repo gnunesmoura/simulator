e0dc4075ec8818d41df4570dc7197ad477e77062
Gustavo Nunes <gustavo.moura@computacao.ufla.br>
2016-11-02 20:12:31 -0200

    Minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,23 @@
-// #include <iostream>
+#include <iostream>
 #include <vector>
 #include <init/node.hpp>
+#include <init/vector.hpp>
 
 using namespace std;
 
 int main () {
-    Node a(0.5, 0.6, Type::anchor);
-    cout << a.get(Axis::x) << endl;
+    cout << "\n\n";
+    
+    Node a(0, 0, Type::anchor);
+    Node b(2, 0, Type::anchor);
 
-    a.setType(Type::placed);
-    cout << a.getType() << endl;
+    Vector v1 (a, b, 2);
+    v1.print();
+    Vector v2 (0, 2);
+    v2.print();
+    Vector v3;
+    v3 = v1 + v3;
+    v3 = v3*8;
+    v3.print();
     return 0;
 }
